3803264d9d9103fc9965e2e3fba2d33c34919bb4
ccanyanwu <eme42c@gmail.com>
2022-02-13 06:40:52 +0100

    feat: update CSS style, delete and check button

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,10 +1,20 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-export const Container =  styled.div`
+export const Container = styled.div`
   height: 500px;
-  width: 250px;
-  background-color: #fff7f7;
-`
+  width: 30vw;
+  padding: 30px 20px;
+  margin-top: 10px;
+  border-radius: 38px;
+  box-shadow: 6px -6px 8px #9d9d9d, -6px 6px 8px #ffffff;
+
+  @media all and (max-width: 768px) {
+     {
+      margin-bottom: 32px;
+      width: 70vw;
+    }
+  }
+`;
 
 export const Heading = styled.div`
   height: 50px;
@@ -12,16 +22,21 @@ export const Heading = styled.div`
   justify-content: center;
   align-items: center;
   background-color: gray;
+  border-radius: 10px;
+  box-shadow: 3px -3px 8px #9d9d9d, -3px 3px 8px #ffffff;
+  color: #ffffff;
+  font-weight: 700;
+  margin-bottom: 24px;
+  text-shadow: 2px -2px 6px #333;
+`;
+function Wrapper({ heading, children }) {
+  return (
+    <Container>
+      <Heading>{heading}</Heading>
 
-
-
-`
-function Wrapper({heading, children}) {
-  return <Container>
-    <Heading>{heading}</Heading>
-
-    {children}
-  </Container>
+      {children}
+    </Container>
+  );
 }
 
-export default Wrapper
+export default Wrapper;
